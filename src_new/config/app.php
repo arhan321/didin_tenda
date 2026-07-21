5d98f3bc54e58657057b85004c8213ddb3d882c0
Didin Tenda Auto Backup <arhanmali96@gmail.com>
2026-07-21 20:35:18 +0000

    auto commit didin_tenda: 2026-07-21 20:35:18

--- src_new/config/app.php
+++ src_new/config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://tenda.djncloud.my.id'),
+    'url' => env('APP_URL', 'https://didintenda.my.id'),
 
     /*
     |--------------------------------------------------------------------------
